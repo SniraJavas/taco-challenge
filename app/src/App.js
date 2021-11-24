4f153fb12978eb75f0d15d2320582f43f8ed2386
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2021-11-24 11:36:10 +0200

    second commit

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {ListResturants} from './Components/listResturants/listResturants';
+
+function App() {
+  return (
+    <div className="App">
+           <ListResturants/>
+    </div>
+  );
+}
+
+export default App;
