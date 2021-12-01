ccc543b7938c6c52a6e5630cdb71ebb549b0b147
unknown <snirajumba@gmail.com>
2021-12-01 12:22:40 -0800

    filtering by city and food type

--- app/src/Components/listResturants/listResturants.js
+++ app/src/Components/listResturants/listResturants.js
@@ -18,11 +18,143 @@ export class ListResturants extends Component {
     }
 
     onSearch = (event) =>{
-        console.log("event ",event.target.value)
-        var value = event.target.value;
-        this.setState({
+        let word = event;
+        let wordArr = word.split('in');
+      
+        if(word.toString() == '' && word.length == 0){
+            this.setState({
+                resturants: [{
+                    "Id": 2380,
+                    "Name": "Barcelos",
+                    "City": "Pretoria",
+                    "Suburb": "Pretoria",
+                    "LogoPath": "/RestaurantImages/barcelos.png",
+                    "Rank": 0,
+                    "Categories": [{
+                        "Name": "Wholesome Vegetarian Meals",
+                        "MenuItems": [{
+                            "Id": 941580,
+                            "Name": "Vegetarian Burger",
+                            "Price": 36.9000
+                        }, {
+                            "Id": 941581,
+                            "Name": "Vegetarian Stir Fry & Rice",
+                            "Price": 42.9000
+                        }, {
+                            "Id": 941582,
+                            "Name": "Vegetarian Schwarma",
+                            "Price": 41.9000
+                        }]
+                    }]
+                },{
+                    "Id": 1361,
+                    "Name": "CHIAPAS eat mexican",
+                    "City": "Johannesburg",
+                    "Suburb": "Rosebank",
+                    "LogoPath": "/RestaurantImages/chiapas.png",
+                    "Rank": 1,
+                    "Categories": [{
+                        "Name": "Mains",
+                        "MenuItems": [{
+                            "Id": 1315807,
+                            "Name": "Crispy Tacos",
+                            "Price": 80.0000
+                        }, {
+                            "Id": 1315808,
+                            "Name": "Soft Tacos",
+                            "Price": 80.0000
+                        }]
+                    }]
+                }, {
+                    "Id": 2438,
+                    "Name": "CHIAPAS eat mexican",
+                    "City": "Pretoria",
+                    "Suburb": "Brooklyn",
+                    "LogoPath": "/RestaurantImages/chiapas.png",
+                    "Rank": 8,
+                    "Categories": [{
+                        "Name": "Mains",
+                        "MenuItems": [{
+                            "Id": 1137091,
+                            "Name": "Crispy Tacos",
+                            "Price": 70.0000
+                        }, {
+                            "Id": 1137092,
+                            "Name": "Soft Tacos",
+                            "Price": 70.0000
+                        }]
+                    }]
+                }]
+            })
+        }else if(wordArr.length > 1 ){
+                
+                 this.setState({resturants:this.filteringByCity(this.state.resturants,wordArr[wordArr.length-1].trim())})
+        }else{
+            if(!wordArr[wordArr.length-1].includes(" i")){
+                this.setState({
+                    resturants : this.filteringByMealName(this.state.resturants,word.trim())
+                })
+            }
+            
+            
+        }
+    }
 
-        })
+    // filteringByResturantName = (arr , name) =>{
+    //     let filterdArr = []
+    //     if(name != null){
+    //         for(var i = 0; i < arr.length; i++)
+    //         {
+    //             if(arr[i].Name.toLowerCase().includes(name.toLowerCase())){
+    //                     filterdArr.push(arr[i])
+
+    //             }
+    //         }
+    //     }
+
+    //     return filterdArr ;
+      
+    // }
+
+    filteringByMealName = (arr , name) =>{
+        let filterdArr = []
+        if(name != ""){
+            for(var i = 0; i < arr.length; i++)
+            {
+                arr[i].Categories[0].MenuItems.forEach(element => {
+                   
+                    if(element.Name.toLowerCase().includes(name.toLowerCase())){
+                        if(!filterdArr.includes(arr[i])){
+                              filterdArr.push(arr[i])
+                        }
+                }
+                });
+            }
+            return filterdArr ;
+        }else{
+            return this.state.resturants;
+        }
+
+     
+      
+    }
+
+    filteringByCity = (arr , city) =>{
+        let filterdArr = []
+        if(city != ""){
+            for(var i = 0; i < arr.length; i++)
+            {
+                if(arr[i].City.toLowerCase().includes(city.toLowerCase())){
+                  
+                        filterdArr.push(arr[i])
+
+                }
+            }
+            return filterdArr ;
+        }
+
+       return this.state.resturants;
+      
     }
 
     onSelectOrder = (event) =>{
@@ -97,11 +229,10 @@ export class ListResturants extends Component {
 
 
     render() {
-        console.log("Resturants ",this.state.resturants)
         let resturantsList = this.state.resturants.map((item,i) => {
                 return (<><tr>
                     <td><img width="25%" height="25%" src={defaultImage} alt="resturant picture" /></td>
-                    <td><h5>{item.Name} - {item.Suburb} - rated# {item.Rank} overall</h5> </td>
+                    <td><h5>{item.Name} - {item.City} - rated# {item.Rank} overall</h5> </td>
                     
                 </tr>{
                         item.Categories.map((item2,i2) =>{
